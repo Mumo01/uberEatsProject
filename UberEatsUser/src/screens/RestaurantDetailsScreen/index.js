6a7e5dcd5a6b7290c4143f39567cfc5125fae275
Victor Mumo <victormumo@Victors-MacBook-Pro.local>
2024-01-16 15:58:16 +0300

    Update with Rest. Details Screen Done

--- UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
+++ UberEatsUser/src/screens/RestaurantDetailsScreen/index.js
@@ -1,60 +1,31 @@
-import {View, Text, Image, FlatList, StyleSheet } from "react-native";
+import {View, FlatList } from "react-native";
 import {Ionicons} from '@expo/vector-icons';
+import DishListItem from "../../components/DishListItem";
 import restaurants from '../../../assets/data/restaurants.json';
+import Header from "./Header";
+import styles from "./styles";
 
 const restaurant = restaurants[0];
-("");
+
 
 const RestaurantDetailsPage = () => {
     return (
         <View style={styles.page}>
 
-            <Image  source={{uri: restaurant.image}} style={styles.image} />
-    
+            <FlatList 
+                ListHeaderComponent={() => <Header restaurant={restaurant}/>}
+                data={ restaurant.dishes }
+                renderItem={({ item }) => <DishListItem dish={item} />} 
+            />
                 <Ionicons name="arrow-back-circle"
                             size={45}
                             color="white"
                             style={styles.iconContainer}
                 />
-            
-            
-            <View style={styles.container}>
-            <Text style={styles.title}>{restaurant.name}</Text>
-            <Text style={styles.subtitle}>
-                $ {restaurant.deliveryFee} &#8226;
-                 {restaurant.minDeliveryTime}-{restaurant.maxDeliveryTime} minutes
-            </Text>
 
-            </View>
         </View>
     );
 };
 
-const styles = StyleSheet.create({
-    page: {
-       flex: 1, 
-    },
-    iconContainer: {
-        position: "absolute",
-        top: 40,
-        left: 10,
-    },
-    image: {
-        width: "100%",
-        aspectRatio: 5 / 3,
-    },
-    title: {
-        fontSize: 35,
-        fontWeight: "600",
-        marginVertical: 10,
-    },
-    subtitle: {
-        color: 'grey',
-        fontSize: 15,
-    },
-    container: {
-        margin: 10,
-    },
-});
 
 export default RestaurantDetailsPage;
